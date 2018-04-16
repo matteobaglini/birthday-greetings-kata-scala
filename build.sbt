8fbeaae50ad13a0287b77ead20ca287a35635cd5
Matteo Baglini <matteo.baglini@gmail.com>
2018-04-16 17:20:20 +0200

    Add Cats Core

--- build.sbt
+++ build.sbt
@@ -25,6 +25,7 @@ lazy val root = (project in file(".")).
     libraryDependencies ++= Seq(
       javaMail,
       javaActivation,
+      catsCore,
       catsEffect,
       miniTest % Test,
       dumbster % Test
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,7 @@ import sbt._
 object Dependencies {
   lazy val javaMail = "javax.mail" % "mail" % "1.4.7"
   lazy val javaActivation = "javax.activation" % "activation" % "1.1.1"
+  lazy val catsCore = "org.typelevel" %% "cats-core" % "1.0.1"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "0.10"
 
   lazy val miniTest = "io.monix" %% "minitest" % "2.1.1"

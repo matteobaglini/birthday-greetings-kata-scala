6cd7e9f3ee69e683d7febf6203a21a54776274c5
Matteo Baglini <matteo.baglini@gmail.com>
2018-04-16 17:39:59 +0200

    Move down the call to first unsafaRunSync

--- src/main/scala/it/kata/birthday_greetings/BirthdayService.scala
+++ src/main/scala/it/kata/birthday_greetings/BirthdayService.scala
@@ -11,10 +11,12 @@ object BirthdayService {
   def sendGreetings(loadEmployees: LoadEmployees,
                     sendMessage: SendMessage,
                     today: XDate): Unit = {
-    val loadIO: IO[List[Employee]] = loadEmployees()
-    val es: List[Employee] = loadIO.unsafeRunSync()
 
-    val birthdays: List[Employee] = es.filter(e => e.isBirthday(today))
+    val loadIO: IO[List[Employee]] = loadEmployees()
+
+    val birthdaysIO: IO[List[Employee]] =
+      loadIO.map(es => es.filter(e => e.isBirthday(today)))
+    val birthdays: List[Employee] = birthdaysIO.unsafeRunSync()
 
     val collapsedSendAllIO: IO[Unit] =
       birthdays.traverse(e => sendMessage(e)).map(_ => ())

70d0b4b9dcec09b327cf7b9d571bc32c8a0c5c0e
Matteo Baglini <matteo.baglini@gmail.com>
2018-04-24 15:37:45 +0200

    More split of fetching/parsing

--- src/main/scala/it/kata/birthday_greetings/Repository.scala
+++ src/main/scala/it/kata/birthday_greetings/Repository.scala
@@ -10,19 +10,19 @@ object Repository {
   }
 
   def buildFileRepositoy(fileName: String): Repository = new Repository {
-    def loadEmployees(): IO[List[Employee]] = IO {
-      val in = new BufferedReader(new FileReader(fileName))
 
+    def loadEmployees(): IO[List[Employee]] =
+      readLines(fileName)
+        .map(_.map(_.split(", ")))
+        .map(_.map(data => Employee(data(1), data(0), data(2), data(3))))
+
+    private def readLines(fileName: String): IO[List[String]] = IO {
+      val in = new BufferedReader(new FileReader(fileName))
       try {
         val lines = new collection.mutable.ListBuffer[String]
-        var str = ""
-        str = in.readLine // skip header
+        var str = in.readLine // skip header
         while ({ str = in.readLine; str != null }) lines += str
-
-        lines
-          .map(_.split(", "))
-          .map(data => Employee(data(1), data(0), data(2), data(3)))
-          .toList
+        lines.toList
       } finally {
         in.close()
       }

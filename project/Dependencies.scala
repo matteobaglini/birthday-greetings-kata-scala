d487f44998251d6ba80a95a7d6ad3cea1be2345c
Matteo Baglini <matteo.baglini@gmail.com>
2018-04-26 17:08:29 +0200

    Update cats dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object Dependencies {
   lazy val javaMail = "javax.mail" % "mail" % "1.4.7"
   lazy val javaActivation = "javax.activation" % "activation" % "1.1.1"
-  lazy val catsCore = "org.typelevel" %% "cats-core" % "1.0.1"
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "0.10"
+  lazy val catsCore = "org.typelevel" %% "cats-core" % "1.1.0"
+  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "1.0.0-RC"
 
   lazy val miniTest = "io.monix" %% "minitest" % "2.1.1"
   lazy val dumbster = "dumbster" % "dumbster" % "1.6"

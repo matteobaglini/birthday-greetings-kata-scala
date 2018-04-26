570f956505467cb284f5441cc53fb999b4a269fb
Matteo Baglini <matteo.baglini@gmail.com>
2018-04-26 16:10:33 +0200

    wip

--- src/main/scala/it/kata/birthday_greetings/BirthdayService.scala
+++ src/main/scala/it/kata/birthday_greetings/BirthdayService.scala
@@ -7,30 +7,34 @@ import javax.mail.{Message, Session, Transport}
 
 import cats.implicits._
 import cats.data.Reader
+import cats.data.ReaderT
+import cats.effect.IO
 
 case class Config(fileName: String, smtpHost: String, smtpPort: Int)
 
 object BirthdayService {
 
-  def sendGreetings(xDate: XDate): Reader[Config, Unit] =
-    loadEmployees()
-      .map(es => es.filter(_.isBirthday(xDate)))
-      .flatMap(es => sendAllGreetings(es))
+  def sendGreetings(xDate: XDate): Reader[Config, Unit] = ???
+  // loadEmployees()
+  //   .map(es => es.filter(_.isBirthday(xDate)))
+  //   .flatMap(es => sendAllGreetings(es))
 
-  def loadEmployees(): Reader[Config, List[Employee]] = Reader { config =>
-    val employees = new collection.mutable.ListBuffer[Employee]
-    val in = new BufferedReader(new FileReader(config.fileName))
-    var str = ""
-    str = in.readLine // skip header
-    while ({ str = in.readLine; str != null }) {
-      val employeeData = str.split(", ")
-      val employee = Employee(employeeData(1),
-                              employeeData(0),
-                              employeeData(2),
-                              employeeData(3))
-      employees += employee
+  def loadEmployees(): ReaderT[IO, Config, List[Employee]] = ReaderT { config =>
+    IO {
+      val employees = new collection.mutable.ListBuffer[Employee]
+      val in = new BufferedReader(new FileReader(config.fileName))
+      var str = ""
+      str = in.readLine // skip header
+      while ({ str = in.readLine; str != null }) {
+        val employeeData = str.split(", ")
+        val employee = Employee(employeeData(1),
+                                employeeData(0),
+                                employeeData(2),
+                                employeeData(3))
+        employees += employee
+      }
+      employees.toList
     }
-    employees.toList
   }
 
   def sendAllGreetings(es: List[Employee]): Reader[Config, Unit] =

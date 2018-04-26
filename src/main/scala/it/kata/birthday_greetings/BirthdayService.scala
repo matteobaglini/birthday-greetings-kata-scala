702d84125cae55f81a7c8954cb15bf51a408e8ab
Matteo Baglini <matteo.baglini@gmail.com>
2018-04-26 14:39:19 +0200

    Use Reader monad locally to every functions

--- src/main/scala/it/kata/birthday_greetings/BirthdayService.scala
+++ src/main/scala/it/kata/birthday_greetings/BirthdayService.scala
@@ -5,37 +5,42 @@ import java.util.Properties
 import javax.mail.internet.{InternetAddress, MimeMessage}
 import javax.mail.{Message, Session, Transport}
 
+import cats.data.Reader
+
 case class Config(fileName: String, smtpHost: String, smtpPort: Int)
 
 object BirthdayService {
 
-  def sendGreetings(config: Config, xDate: XDate): Unit = {
-    loadEmployees(config)
-      .filter(_.isBirthday(xDate))
-      .map(sendGreetings(config, _))
-  }
+  def sendGreetings(config: Config, xDate: XDate): Unit =
+    Reader[Config, Unit] { config =>
+      loadEmployees(config)
+        .filter(_.isBirthday(xDate))
+        .map(sendGreetings(config, _))
+    }.run(config)
 
-  def loadEmployees(config: Config): List[Employee] = {
-    val employees = new collection.mutable.ListBuffer[Employee]
-    val in = new BufferedReader(new FileReader(config.fileName))
-    var str = ""
-    str = in.readLine // skip header
-    while ({ str = in.readLine; str != null }) {
-      val employeeData = str.split(", ")
-      val employee = Employee(employeeData(1),
-                              employeeData(0),
-                              employeeData(2),
-                              employeeData(3))
-      employees += employee
-    }
-    employees.toList
-  }
+  def loadEmployees(config: Config): List[Employee] =
+    Reader[Config, List[Employee]] { config =>
+      val employees = new collection.mutable.ListBuffer[Employee]
+      val in = new BufferedReader(new FileReader(config.fileName))
+      var str = ""
+      str = in.readLine // skip header
+      while ({ str = in.readLine; str != null }) {
+        val employeeData = str.split(", ")
+        val employee = Employee(employeeData(1),
+                                employeeData(0),
+                                employeeData(2),
+                                employeeData(3))
+        employees += employee
+      }
+      employees.toList
+    }.run(config)
 
-  def sendGreetings(config: Config, employee: Employee): Unit = {
-    val session = buildSession(config.smtpHost, config.smtpPort)
-    val msg = buildMessage(employee, session)
-    Transport.send(msg)
-  }
+  def sendGreetings(config: Config, employee: Employee): Unit =
+    Reader[Config, Unit] { config =>
+      val session = buildSession(config.smtpHost, config.smtpPort)
+      val msg = buildMessage(employee, session)
+      Transport.send(msg)
+    }.run(config)
 
   private def buildSession(smtpHost: String, smtpPort: Int): Session = {
     val props = new Properties

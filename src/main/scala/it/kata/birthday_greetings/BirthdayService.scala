dc5a4ea7b7f62feab95ce7a949f644461b9d29ed
Matteo Baglini <matteo.baglini@gmail.com>
2018-04-27 10:40:28 +0200

    Implements domain type classes

--- src/main/scala/it/kata/birthday_greetings/BirthdayService.scala
+++ src/main/scala/it/kata/birthday_greetings/BirthdayService.scala
@@ -12,13 +12,88 @@ import cats.effect._
 
 case class Config(fileName: String, smtpHost: String, smtpPort: Int)
 
-trait EmployeeRepository[F[_]] {
-  def loadEmployees(): F[List[Employee]]
+object EmployeeRepository {
+
+  trait EmployeeRepository[F[_]] {
+    def loadEmployees(): F[List[Employee]]
+  }
+
+  class FlatFileEmployeeRepository[F[_]](implicit
+                                         MR: ApplicativeAsk[F, Config],
+                                         S: Sync[F])
+      extends EmployeeRepository[F] {
+
+    def loadEmployees(): F[List[Employee]] =
+      for {
+        config <- MR.ask
+        lines <- loadLines(config.fileName)
+        es = lines.map(l => parse(l))
+      } yield es
+
+    private def loadLines(fileName: String): F[List[String]] = S.delay {
+      val lines = new collection.mutable.ListBuffer[String]
+      val in = new BufferedReader(new FileReader(fileName))
+      var str = in.readLine // skip header
+      while ({ str = in.readLine; str != null }) {
+        lines += str
+      }
+      lines.toList
+    }
+
+    private def parse(line: String): Employee = {
+      val data = line.split(", ")
+      Employee(data(1), data(0), data(2), data(3))
+    }
+  }
+
 }
 
-trait GreetingsGateway[F[_]] {
-  def sendAllGreetings(es: List[Employee]): F[List[Employee]]
-  def send(e: Employee): F[Unit]
+object GreetingsGateway {
+
+  trait GreetingsGateway[F[_]] {
+    def sendAllGreetings(es: List[Employee]): F[Unit]
+    def send(e: Employee): F[Unit]
+  }
+
+  class SmtpGreetingsGateway[F[_]](implicit MR: ApplicativeAsk[F, Config],
+                                   S: Sync[F])
+      extends GreetingsGateway[F] {
+
+    def sendAllGreetings(es: List[Employee]): F[Unit] =
+      es.traverse_(e => send(e))
+
+    def send(e: Employee): F[Unit] =
+      for {
+        config <- MR.ask
+        _ <- sendMessage(config.smtpHost, config.smtpPort, e)
+      } yield ()
+
+    private def sendMessage(smtpHost: String,
+                            smtpPort: Int,
+                            e: Employee): F[Unit] = S.delay {
+      val session = buildSession(smtpHost, smtpPort)
+      val msg = buildMessage(e, session)
+      Transport.send(msg)
+    }
+
+    private def buildSession(smtpHost: String, smtpPort: Int): Session = {
+      val props = new Properties
+      props.put("mail.smtp.host", smtpHost)
+      props.put("mail.smtp.port", "" + smtpPort)
+      Session.getInstance(props, null)
+    }
+
+    private def buildMessage(employee: Employee,
+                             session: Session): MimeMessage = {
+      val msg = new MimeMessage(session)
+      msg.setFrom(new InternetAddress("sender@here.com"))
+      msg.setRecipient(Message.RecipientType.TO,
+                       new InternetAddress(employee.email))
+      msg.setSubject("Happy Birthday!")
+      msg.setText(s"Happy Birthday, dear ${employee.firstName}!");
+      msg
+    }
+  }
 }
 
 object BirthdayService {

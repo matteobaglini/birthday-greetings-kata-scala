eed3eed8f7bf96a5ad43f4303885059c35138f27
Matteo Baglini <matteo.baglini@gmail.com>
2018-05-08 15:52:04 +0200

    Implement FlatFileEmployeeRepository adapter

--- src/main/scala/it/kata/birthday_greetings/FlatFileEmployeeRepository.scala
+++ src/main/scala/it/kata/birthday_greetings/FlatFileEmployeeRepository.scala
@@ -6,6 +6,27 @@ object FlatFileEmployeeRepository {
 
   def fromFile(fileName: String): EmployeeRepository = new EmployeeRepository {
 
-    def loadEmployees(): IO[List[Employee]] = ???
+    def loadEmployees(): IO[List[Employee]] = {
+      loadLines().map { lines =>
+        lines
+          .drop(1) // skip header
+          .map(parseEmployee(_))
+      }
+    }
+
+    private def loadLines(): IO[List[String]] =
+      IO(io.Source.fromFile(fileName)).bracket { source =>
+        IO(source.getLines.toList)
+      } { source =>
+        IO(source.close)
+      }
+
+    private def parseEmployee(line: String): Employee = {
+      val employeeData = line.split(", ")
+      Employee(employeeData(1),
+               employeeData(0),
+               employeeData(2),
+               employeeData(3))
+    }
   }
 }

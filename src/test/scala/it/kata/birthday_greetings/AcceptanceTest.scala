ec0bf78351a358fa70a01a407e8fad7fbf42b7b1
Matteo Baglini <matteo.baglini@gmail.com>
2018-05-08 18:44:33 +0200

    Use State monad as in memory store for acceptance tests

--- src/test/scala/it/kata/birthday_greetings/AcceptanceTest.scala
+++ src/test/scala/it/kata/birthday_greetings/AcceptanceTest.scala
@@ -1,56 +1,56 @@
 package it.kata.birthday_greetings
 
 import minitest._
-import cats.effect.IO
+import cats.data._
 
 import BirthdayService._
 
 object AcceptanceTest extends SimpleTestSuite {
 
-  class FakeEmployeeRepository(es: List[Employee])
-      extends EmployeeRepository[IO] {
-    def loadEmployees(): IO[List[Employee]] = IO {
-      es
-    }
+  case class TestData(loaded: List[Employee],
+                      receivers: List[Employee] = List())
+
+  type TestResult[A] = State[TestData, A]
+
+  class InMemoryEmployeeRepository() extends EmployeeRepository[TestResult] {
+
+    def loadEmployees(): TestResult[List[Employee]] =
+      State.get.map(_.loaded)
   }
 
-  class FakeMessageGateway extends MessageGateway[IO] {
+  class InMemoryMessageGateway extends MessageGateway[TestResult] {
 
-    val receivers = new collection.mutable.ListBuffer[Employee]
-
-    def sendMessage(e: Employee): IO[Unit] = IO {
-      receivers += e
-    }
+    def sendMessage(e: Employee): TestResult[Unit] =
+      State.modify(s => s.copy(receivers = s.receivers :+ e))
   }
+
+  implicit val employeeRepository = new InMemoryEmployeeRepository()
+  implicit val messageGateway = new InMemoryMessageGateway()
 
   test("will send greetings when its somebody's birthday") {
     val john = Employee("John", "Doe", "1982/10/08", "john.doe@foobar.com")
     val mary = Employee("Mary", "Ann", "1975/03/11", "mary.ann@foobar.com")
-    val employees = List(john, mary)
+    val employees = TestData(List(john, mary))
     val today = XDate("2008/10/08")
 
-    implicit val fakeEmployeeRepository = new FakeEmployeeRepository(employees)
-    implicit val fakeMessageGateway = new FakeMessageGateway()
+    val result = sendGreetings[TestResult](today)
+      .runS(employees)
+      .value
 
-    sendGreetings[IO](today)
-      .unsafeRunSync()
-
-    assert(fakeMessageGateway.receivers.size == 1, "message not sent?")
-    assert(fakeMessageGateway.receivers.contains(john), "to wrong employee?")
+    assert(result.receivers.size == 1, "message not sent?")
+    assert(result.receivers.contains(john), "to wrong employee?")
   }
 
   test("will not send emails when nobody's birthday") {
     val john = Employee("John", "Doe", "1982/10/08", "john.doe@foobar.com")
     val mary = Employee("Mary", "Ann", "1975/03/11", "mary.ann@foobar.com")
-    val employees = List(john, mary)
+    val employees = TestData(List(john, mary))
     val today = XDate("2008/01/01")
 
-    implicit val fakeEmployeeRepository = new FakeEmployeeRepository(employees)
-    implicit val fakeMessageGateway = new FakeMessageGateway()
+    val result = sendGreetings[TestResult](today)
+      .runS(employees)
+      .value
 
-    sendGreetings[IO](today)
-      .unsafeRunSync()
-
-    assert(fakeMessageGateway.receivers.size == 0, "what? messages?")
+    assert(result.receivers.size == 0, "what? messages?")
   }
 }

7a9c24142590fdd294b0503ec258ada0cbf41f77
Matteo Baglini <matteo.baglini@gmail.com>
2018-05-08 16:03:24 +0200

    Implement SmtpMessageGateway adapter

--- src/main/scala/it/kata/birthday_greetings/SmtpMessageGateway.scala
+++ src/main/scala/it/kata/birthday_greetings/SmtpMessageGateway.scala
@@ -1,5 +1,9 @@
 package it.kata.birthday_greetings
 
+import java.util.Properties
+import javax.mail.internet.{InternetAddress, MimeMessage}
+import javax.mail.{Message, Session, Transport}
+
 import cats.effect._
 
 object SmtpMessageGateway {
@@ -7,6 +11,33 @@ object SmtpMessageGateway {
   def fromEndpoint(smtpHost: String, smtpPort: Int): MessageGateway =
     new MessageGateway {
 
-      def sendMessage(employee: Employee): IO[Unit] = ???
+      def sendMessage(employee: Employee): IO[Unit] = IO {
+        val session = buildSession()
+        val msg = buildMessage(session, employee)
+        Transport.send(msg)
+      }
+
+      private def buildSession(): Session = {
+        val props = new Properties
+        props.put("mail.smtp.host", smtpHost)
+        props.put("mail.smtp.port", "" + smtpPort)
+        Session.getInstance(props, null)
+      }
+
+      private def buildMessage(session: Session,
+                               employee: Employee): MimeMessage = {
+        val recipient = employee.email
+        val sender = "sender@here.com"
+        val body = s"Happy Birthday, dear ${employee.firstName}!"
+        val subject = "Happy Birthday!"
+
+        val msg = new MimeMessage(session)
+        msg.setFrom(new InternetAddress(sender))
+        msg.setRecipient(Message.RecipientType.TO,
+                         new InternetAddress(recipient))
+        msg.setSubject(subject)
+        msg.setText(body);
+        msg
+      }
     }
 }

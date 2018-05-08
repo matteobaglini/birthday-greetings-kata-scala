8c01d550cefbc6241cdbfc719595d8502182a7af
Matteo Baglini <matteo.baglini@gmail.com>
2018-05-08 15:59:06 +0200

    Define MessageGateway port

---
+++ src/main/scala/it/kata/birthday_greetings/MessageGateway.scala
@@ -0,0 +1,13 @@
+package it.kata.birthday_greetings
+
+import cats.implicits._
+import cats.effect._
+
+trait MessageGateway {
+
+  def sendMessages(employees: List[Employee]): IO[Unit] = {
+    employees.traverse_(sendMessage(_))
+  }
+
+  def sendMessage(employee: Employee): IO[Unit]
+}
